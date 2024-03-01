611e27e04df2433414e88b271b0b3162f9513788
kalaiselvanj <110382458+kalaiselvanj@users.noreply.github.com>
2024-03-01 11:23:59 +0530

    Update settings.py

--- exam_project/settings.py
+++ exam_project/settings.py
@@ -169,6 +169,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kalaiselvanj@systechusa.com'  # Replace with your Gmail address
-EMAIL_HOST_PASSWORD = 'M1cr0A((0fK@l@1'  # Replace with your Gmail password or an app-specific password
+EMAIL_HOST_USER = 'sysconnect@systechusa.com'  # Replace with your Gmail address
+EMAIL_HOST_PASSWORD = '$ys7ech1@3'  # Replace with your Gmail password or an app-specific password
 
